5759a6d700dedd26c2c583bcd81293b2d0af1f89
Olga <o2-2010@mail.ru>
2021-09-15 21:14:24 +0300

    nio

--- io/src/main/kotlin/ru/sber/nio/Grep.kt
+++ io/src/main/kotlin/ru/sber/nio/Grep.kt
@@ -1,5 +1,13 @@
 package ru.sber.nio
 
+import io.mockk.InternalPlatformDsl.toArray
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.io.path.useLines
+import kotlin.streams.toList
+
 /**
  * Реализовать простой аналог утилиты grep с использованием калссов из пакета java.nio.
  */
@@ -15,6 +23,20 @@ class Grep {
      * 22-01-2001-1.log : 3 : 192.168.1.1 - - [22/Jan/2001:14:27:46 +0000] "POST /files HTTP/1.1" 200 - "-"
      */
     fun find(subString: String) {
+        val path = Paths.get("io", "logs").toAbsolutePath()
 
+        val outFile = Files.createFile( Paths.get("io", "result.txt"))
+        var fileList = Files.find(path, 10, { p, _ -> p.toString().endsWith(".log") })
+
+        Files.newBufferedWriter(outFile).use { out ->
+            for (filePath in fileList) {
+                var lines = Files.lines(filePath).toList()
+                for (i in 0..lines.size - 1) {
+                    var line = lines.get(i)
+                    if (line.contains(subString))
+                        out.write("${filePath.fileName} : ${i + 1} : ${line} \n")
+                }
+            }
+        }
     }
 }
